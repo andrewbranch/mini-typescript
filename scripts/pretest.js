803a30e8fc0571977cab99322758ca86f10f336a
Andrew Branch <andrew@wheream.io>
2022-04-04 15:58:52 -0700

    Remove semicolon from package.json script

--- scripts/pretest.js
+++ scripts/pretest.js
@@ -1,13 +1,18 @@
 // @ts-check
+const { execSync } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
+console.log('npm run build')
+execSync('npm run build', { stdio: 'inherit' })
+
+console.log('rm -rf baselines/local')
 rimraf(path.join(__dirname, '../baselines/local'))
 
 function rimraf(path) {
   if (fs.existsSync(path)) {
     fs.readdirSync(path).forEach(file => {
-      const curPath = path + "/" + file
+      const curPath = path + '/' + file
       if (fs.lstatSync(curPath).isDirectory()) {
         rimraf(curPath)
       } else {
